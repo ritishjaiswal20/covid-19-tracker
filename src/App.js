638d646a3b8ffd84540428fe01f2137552e8e656
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-02-05 11:56:03 +0530

    fetched api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import FormControl from '@mui/material/FormControl'; 
+import { MenuItem,Select} from '@mui/material';
+import { useEffect, useState } from 'react';
+//https://disease.sh/v3/covid-19/countries
 
 function App() {
+  const[countries,setCountries]=useState([]);
+  const[country,setCountry]=useState("world wide")
+  useEffect(()=>{
+    const getCountriesData= async () =>{
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response)=>response.json())
+      .then((data)=>{
+        const countries= data.map((country)=>(
+          {
+            name:country.country,
+            value:country.countryInfo.iso2,
+        })
+        );
+        setCountries(countries);
+      })
+    }
+    getCountriesData();
+  },[]);
+
+  const onCountryChange = async (event)=>{
+    const countryCode= event.target.value;
+    console.log("country-code--",countryCode);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app_header">
+      <h1>covid 19 tracker</h1>
+      <FormControl>
+         <Select variant="outlined" onChange={onCountryChange}value={country}>
+         <MenuItem value="World-wide">Worldwide</MenuItem>
+          {
+            countries.map((country)=>(
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))
+          }
+         </Select>
+      </FormControl>
+      </div>
     </div>
   );
 }
